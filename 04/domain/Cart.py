773465ee7be17db7500e07865659bb4184598697
Meek Msaki <meek10x@gmail.com>
2023-12-05 16:27:07 -0600

    remove unecessary condition (#4)

--- 04/domain/Cart.py
+++ 04/domain/Cart.py
@@ -11,16 +11,15 @@ class Cart:
     if item: return self.cart.append(item)
 
   def remove(self, item: Item):
-    if item:
-      product = item.product
-      quantity = item.quantity
-      for i, v in enumerate(self.cart):
-        if v.product == product:
-          self.cart[i] -= quantity
-          if self.cart[i].quantity == 0 and len(self.cart[i:]) == 1:
-            self.cart = self.cart[:i]
-          elif self.cart[i].quantity == 0 and len(self.cart[i:]) > 1:
-            self.cart = self.cart[:i] + self.cart[i+1:]
+    product = item.product
+    quantity = item.quantity
+    for i, v in enumerate(self.cart):
+      if v.product == product:
+        self.cart[i] -= quantity
+        if self.cart[i].quantity == 0 and len(self.cart[i:]) == 1:
+          self.cart = self.cart[:i]
+        elif self.cart[i].quantity == 0 and len(self.cart[i:]) > 1:
+          self.cart = self.cart[:i] + self.cart[i+1:]
   
   def get_products(self):
     return self.cart
