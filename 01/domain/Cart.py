23f6db493f07f22cb0c351f7c5cc70586278b6c2
Meek Msaki <meek10x@gmail.com>
2023-12-05 05:37:57 -0600

    fix conditional to check length

--- 01/domain/Cart.py
+++ 01/domain/Cart.py
@@ -12,7 +12,7 @@ class Cart:
     self.cart.append(item.product)
   
   def get_products(self):
-    if self.cart == 1:
+    if self.cart and len(self.cart) == 1:
       return "".join(self.cart)
     else:
       return ", ".join(self.cart[0:-1]) + " and " + self.cart[-1]
