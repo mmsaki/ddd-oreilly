f21e6df0c6bb6e23b6d3d62f986fe5b777a0d90f
Meek Msaki <meek10x@gmail.com>
2023-12-05 05:34:10 -0600

    remove duplicate items

--- 01/app.py
+++ 01/app.py
@@ -4,9 +4,6 @@ from domain.Product import Product
 cart = Cart()
 product = Product(name="Apple Pencil")
 cart.add(product)
-cart.add(product)
-cart.add(product)
-cart.add(product)
 
 
 print(cart.get_products())
